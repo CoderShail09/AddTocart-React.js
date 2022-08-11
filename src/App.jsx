1d83053718f0897f2eb7fbe473ad046fa7abcfed
shailendra singh <ssin09559@gmail.com>
2022-08-11 11:58:10 +0530

    project completed

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import Header from "./components/Header";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Cart from "./components/Cart";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Header />
+      <div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/" />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+export default App;
